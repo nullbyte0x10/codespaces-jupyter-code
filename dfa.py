e337f6ea1fcbe70d27e10c836a7d37d119a75643
trevormalesike <120114568+trevormalesike@users.noreply.github.com>
2023-02-16 11:46:49 +0000

    correct dfa

--- dfa.py
+++ dfa.py
@@ -1,62 +1,46 @@
-class DFA:
-    def __init__(self, alphabet, transitions, start_state, accepting_states):
-        self.alphabet = alphabet
-        self.transitions = transitions
-        self.start_state = start_state
-        self.accepting_states = accepting_states
-
-    def accepts_language1(self, string):
-        state = self.start_state
-        for symbol in string:
-            state = self.transitions.get((state, symbol))
-            if state is None:
-                return False
-        return state in self.accepting_states
-
-    def accepts_language2(self, string):
-        state = self.start_state
-        for symbol in string:
-            state = self.transitions.get((state, symbol))
-            if state is None:
-                return False
-            if state == 1 and symbol == 'b':
+# Define the DFA
+def dfa(input_string):
+    state = 0
+    for symbol in input_string:
+        if state == 0:
+            if symbol == 'a':
+                state = 1
+            elif symbol == 'b':
+                state = 3
+        elif state == 1:
+            if symbol == 'a':
                 state = 2
-        return state in self.accepting_states
+            elif symbol == 'b':
+                state = 3
+        elif state == 2:
+            if symbol == 'a':
+                state = 1
+            elif symbol == 'b':
+                state = 3
+        elif state == 3:
+            if symbol == 'a':
+                state = 4
+            elif symbol == 'b':
+                state = 3
+        elif state == 4:
+            if symbol == 'a':
+                state = 2
+            elif symbol == 'b':
+                state = 3
+    return state
 
-# Example usage for the languages "ends with aa" and "contains ab"
-alphabet = {'a', 'b'}
-transitions = {
-    (1, 'a'): 2,
-    (1, 'b'): 1,
-    (2, 'a'): 3,
-    (2, 'b'): 1,
-    (3, 'a'): 2,
-    (3, 'b'): 4,
-    (4, 'a'): 4,
-    (4, 'b'): 4,
-}
-start_state = 1
-accepting_states1 = {3}
-accepting_states2 = {2, 4}
-
-# dfa = DFA(alphabet, transitions, start_state, accepting_states1)
-
-# while True:
-#     string = input("Enter a string: ")
-#     if string == 'Q':
-#         break
-#     if dfa.accepts_language1(string):
-#         print("String ends with 'aa'")
-#     else:
-#         print("String does not end with 'aa'")
-
-dfa = DFA(alphabet, transitions, start_state, accepting_states2)
-
+# Prompt the user for input until Q is entered
 while True:
-    string = input("Enter a string: ")
-    if string == 'Q':
+    user_input = input("Enter a string (or Q to quit): ")
+    if user_input == 'Q':
         break
-    if dfa.accepts_language2(string):
-        print("String contains 'ab'")
+    result1 = dfa(user_input[-2:]) in [2, 4] # check if the last two characters are 'aa'
+    result2 = 'aba' in user_input # check if the substring 'aba' is present
+    if result1 and result2:
+        print("The input string belongs to both languages.")
+    elif result1:
+        print("The input string belongs to the first language.")
+    elif result2:
+        print("The input string belongs to the second language.")
     else:
-        print("String does not contain 'ab'")
+        print("The input string does not belong to either language.")
