e65bf13c484f2fc1607175ffba321a8cde8f5db8
trevormalesike <malesike@proton.me>
2023-02-23 09:20:12 +0000

    okay

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,3 +1,25 @@
 
 import math
 import random
+Q_LIMIT=100
+BUSY=1
+IDLE=0
+# Global variables
+next_event_type = 0
+num_custs_delayed = 0
+num_delays_required = 0
+num_events = 0
+num_in_q = 0
+server_status = 0
+area_num_in_q = 0.0
+area_server_status = 0.0
+mean_interarrival = 0.0
+mean_service = 0.0
+sim_time = 0.0
+time_arrival = [0.0] * (Q_LIMIT + 1)
+time_last_event = 0.0
+time_next_event = [0.0] * 3
+total_of_delays = 0.0
+infile = None
+outfile = None
+# Function prototypes
