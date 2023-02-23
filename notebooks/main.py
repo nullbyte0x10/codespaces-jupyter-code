4c7a72e5d3799244d5b05c4b6ecf9d4b7a43adfe
trevormalesike <malesike@proton.me>
2023-02-23 09:54:11 +0000

    runs like a charm

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,9 +1,10 @@
-
 import math
 import random
-Q_LIMIT=100
-BUSY=1
-IDLE=0
+from typing import TextIO
+
+Q_LIMIT = 100
+BUSY = 1
+IDLE = 0
 # Global variables
 next_event_type = 0
 num_custs_delayed = 0
@@ -22,4 +23,62 @@ time_next_event = [0.0] * 3
 total_of_delays = 0.0
 infile = None
 outfile = None
+
+
 # Function prototypes
+def initialize():
+    pass
+
+
+def timing():
+    pass
+
+
+def arrive():
+    pass
+
+
+def depart():
+    pass
+
+
+def report():
+    pass
+
+
+def update_time_avg_stats():
+    pass
+
+
+def expon(mean):
+    """"
+    Generate a random number from an exponential distribution with a given mean
+
+    Args:
+        mean(float): The mean of exponential distribution.
+
+    Returns:
+        float: A random number from exponential distribution
+    """
+
+    return random.expovariate(1.0 / mean)
+
+
+# the main function
+def main():
+    # open the input and output files
+    infile: TextIO = open("mm1.out", "r")
+    outfile: TextIO = open("mm1.out", "w")
+    # specify number of events for timing function
+    num_events = 2
+
+    # read input parameters
+    with open(infile) as infile:
+        # read first line and split in 3 parts
+        line = infile.readline().strip()
+        parts = line.split()
+        # convert each to appropriate data type
+        mean_interarrival: float = float(parts[0])
+        mean_service: float = float(parts[1])
+        num_delays_required: int = int(parts[2])
+    # write report heading an input parameters
